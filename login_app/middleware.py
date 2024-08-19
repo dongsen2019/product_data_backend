eabc06a2d3dc8450bad0f4cb66553767e7bc93a6
dongsen <378250523@qq.com>
2024-08-19 17:04:29 +0800

    添加图片搜索栏

--- login_app/middleware.py
+++ login_app/middleware.py
@@ -11,11 +11,11 @@ class SessionValidationMiddleware:
     def __call__(self, request):
         # 获取当前请求路径
         current_path = request.path_info
-        print(current_path)
+        # print(current_path)
+        print(request.session.get('user'))
 
-        # 如果用户未登录且当前路径不是登录页面，重定向到登录页面
-        if not request.session.get('user') and current_path != settings.LOGIN_URL:  # 少了右边的条件就会进入无限重定向循环
-            # return redirect(settings.LOGIN_URL)
+        # 如果用户未登录且当前路径不是登录页面，重定向到登录页面  not True = False \ None 不要混淆 not 和 取非 !
+        if not request.session.get('user', None) and current_path != settings.LOGIN_URL:  # 少了右边的条件就会进入无限重定向循环
             return redirect(settings.LOGIN_URL)
 
         # 继续处理请求
--- login_app/views.py
+++ login_app/views.py
@@ -1,5 +1,4 @@
 import datetime
-
 from django.shortcuts import render, HttpResponse, redirect
 from login_app.models import *
 from django.db.models import Q, F
@@ -10,36 +9,49 @@ from django.contrib.sessions.models import Session
 
 def index(request):
     if request.method == "GET":
-
         response_home = redirect('product_display:index')
 
-        user_session = request.session.get('user', None)
-        print(request.session.values())
-        print(request.session.session_key)
+        user_session = request.session.session_key
+        # print(f"user_session是否为空:{user_session}")
+        # print(request.session.values())
 
-        print(Session.objects.values())
-        for s in Session.objects.values():
-            print(s['expire_date'] > datetime.datetime.now())
+        # 如果 user_session
+        if user_session is not None:
+            for s in Session.objects.values():
+                # print(f"1:{s["session_key"] == user_session}")
+                # print(f"2:{s["expire_date"] > datetime.datetime.now()}")
+                if s["session_key"] == user_session:
+                    if s["expire_date"] > datetime.datetime.now():
+                        return response_home
+                    else:
+                        # request.session.delete() # 过期的session,django在重定向后自动清理
+                        return render(request, 'login_app/login/login.html')
 
-        if user_session is None:
+        # user_session = request.session.get('user', None) # 从session中获取曾今设置过的用户信息user
 
-            print(request.GET.get("account"))
-            print(request.GET.get("pwd"))
+        # print(request.session.values())
+        # print(request.session.session_key)
 
-            if request.GET.get("account") is None and request.GET.get("pwd") is None:
-                return render(request, 'login_app/login/login.html')
-            else:
-                if User.objects.filter(Q(account=request.GET.get("account")) & Q(password=request.GET.get("pwd"))).exists():
-                    request.session['user'] = {'account': request.GET.get("account"), 'pwd': request.GET.get("pwd"), }
-                    return response_home
+        # print(Session.objects.values())
+
+        elif user_session is None:
+            # print(request.GET.get("account"))  账号
+            # print(request.GET.get("pwd"))  密码
+
+            if user_session is None:
+                if request.GET.get("account") is None and request.GET.get("pwd") is None:
+                    return render(request, 'login_app/login/login.html')
                 else:
-                    context = {
-                        'pwd_error': '<span class="hint" style="display: block; color: red; margin-top: 23px">密码错误</span>'
-                    }
-                    return render(request, 'login_app/login/login.html', context=context)
-
-        else:
-            return response_home
+                    if User.objects.filter(Q(account=request.GET.get("account")) & Q(password=request.GET.get("pwd"))).exists():
+                        request.session['user'] = {'account': request.GET.get("account"), 'pwd': request.GET.get("pwd"), }
+                        return response_home
+                    else:
+                        context = {
+                            'pwd_error': '<span class="hint" style="display: block; color: red; margin-top: 23px">密码错误</span>'
+                        }
+                        return render(request, 'login_app/login/login.html', context=context)
+            else:
+                return HttpResponse("session表中, 无此session_key")
 
     elif request.method == "POST":
         print(request.POST)
